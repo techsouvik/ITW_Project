07e246396d7e5c179b9a53726b7802ab3cb826a9
souvik <souvikaasds01@gmail.com>
2021-12-07 15:29:08 +0530

    txts added

--- convert_to_txt.py
+++ convert_to_txt.py
@@ -2,18 +2,25 @@ import PyPDF2
 
 yearacl = input("enter a year")
 
-yearc = [207,219,248]
-def PDFtoTXT(i):
-    pdf = open(f'./'+yearacl+'/PDFs/Document{i}.pdf', 'rb')
+if yearacl == '2007':
+    yearc = 207
+elif yearacl == '2008':
+    yearc = 219
+elif yearacl == '2009':
+    yearc = 248
+
+# yearc = [207,219,248]
+def PDFtoTXT(i,acl):
+    pdf = open(f'./'+acl+'/PDFs/Document'+str(i)+'.pdf', 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdf)
     print(pdfReader.numPages)
     for pageno in range(pdfReader.numPages):
         page = pdfReader.getPage(pageno)
-        with open(f"./"+yearacl+"/TXTs/Document{i}.txt", "a") as txt:
+        with open(f"./"+acl+"/TXTs/Document"+str(i)+".txt", "a") as txt:
             try:
                 txt.write(page.extractText())
             except UnicodeEncodeError:
                 pass
 
-for pdf in range(5):
-    PDFtoTXT(pdf)
+for pdf in range(1,int(yearc)+1):
+    PDFtoTXT(pdf,yearacl)
